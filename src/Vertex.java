249a4ce5af5096db354cab261141e635d72d3a09
brookshank <Joseph.Brooksbank@du.edu>
2018-11-07 20:03:34 -0700

    Changed weights to be stored in a map

--- src/Vertex.java
+++ src/Vertex.java
@@ -16,17 +16,20 @@ import java.util.stream.Collectors;
 public class Vertex implements Comparable {
 
   static double w(Vertex u, Vertex v){
-    for (WeightedVertex anEdge : u.adj){
-      if (anEdge.vertex.equals(v)){
-        return anEdge.weight;
-      }
-    }
-    throw new IllegalStateException("No edge between these vertices");
+//    for (WeightedVertex anEdge : u.adj){
+//      if (anEdge.vertex.equals(v)){
+//        return anEdge.weight;
+//      }
+//    }
+//    throw new IllegalStateException("No edge between these vertices");
+
+    return u.weights.get(v);
   }
   // Constructor
   Vertex(String id_) {
     id = id_;
     adj = new LinkedList<>();
+    weights = new HashMap<>();
     d = -1;
     state = State.UNKNOWN;
     p = PNIL;
@@ -40,6 +43,7 @@ public class Vertex implements Comparable {
     p = other.p;
     // Perform a deep copy (create all new vertices which are copies)
     adj = other.adj.stream().map(WeightedVertex::new).collect(Collectors.toList());
+    weights = (HashMap<Vertex, Double>) other.weights.clone();
   }
   
   // Vertex ID and data
@@ -62,10 +66,12 @@ public class Vertex implements Comparable {
   
   // List of edges 
   public final List<WeightedVertex> adj;  // adjacent vertices
+  public final HashMap<Vertex, Double> weights; // Map of weights for adjacent vertices
   // Add an edge to the graph from this vertex
   public void AddEdge(Vertex dest, double weight) {
 
     adj.add(new WeightedVertex(dest, weight));
+    weights.put(dest, weight);
   }
 
 
